941be21646be1b595c2e464dfcf3b7ab029f78b5
heybrettbell <brett@grassrootsonline.ca>
2017-07-25 13:45:42 -0400

    Added error messages

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,11 +1,15 @@
 class Picture < ApplicationRecord
 
+  validates :artist, presence: true
+  validates :title, length:{minimum: 3, maximum: 20}
+  validates :url, uniqueness: true, presence: true
+
   def self.pictures_created_in_year(year)
-    Picture.where("created_at LIKE ?", "%#{year}%")
+    where("created_at LIKE ?", "%#{year}%")
   end
 
   def self.three_months_ago
-    Picture.where("created_at < ?", 3.months.ago)
+    where("created_at < ?", 3.months.ago)
   end
 
 end
